544b010c31d36294fac57e397aa88216babfea8d
Kevin DeLand <kevin.deland@gmail.com>
2017-08-10 16:37:28 -0400

    😎slideshow! 😎

--- js/agency.js
+++ js/agency.js
@@ -30,5 +30,18 @@
         }
     })
 
+        // Kevin and Sarah's slideshow
+        $("#slideshow > div:gt(0)").hide();
+    
+    setInterval(function() {
+            $('#slideshow > div:first')
+                .fadeOut(1000)
+                .next()
+                .fadeIn(1000)
+                .end()
+                .appendTo('#slideshow');
+        }, 3000);
+        });
+
 
 })(jQuery); // End of use strict
